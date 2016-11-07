1463363717391d6cd3fe6bf37ebf325a52559832
CWPGithub <kennycheungc@gmail.com>
2016-11-07 16:38:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -246,7 +246,7 @@ function resetHighLight (e) {
 var geoJsonLayer = 0;
 /* POLYGON OVERLAY */
 // load polygon geojson, using data to define fillColor, from local directory
-$.getJSON("map test2.geojson", function (data) {   
+$.getJSON("map and town links 16-17.geojson", function (data) {   
   geoJsonLayer = L.geoJson(data, {
     style: style,
     onEachFeature: function( feature, layer) {
